656ee5234ace51ecbcc3e80bd80b75daf946b7e2
ash <tkdgys1234@gmail.com>
2025-04-30 19:00:47 +0900

    Feat: swagger 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { LocationModule } from './location/location.module';
-import { TravelModule } from './travel/travel.module';
-import { PostsModule } from './posts/posts.module';
-import { CommentModule } from './comment/comment.module';
-import { AlbumModule } from './album/album.module';
-import { LikeModule } from './like/like.module';
-import { ReportModule } from './report/report.module';
-import { PaymentsModule } from './payments/payments.module';
-import { AdminModule } from './admin/admin.module';
-import { NoticeModule } from './notice/notice.module';
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule, LocationModule, TravelModule, PostsModule, CommentModule, AlbumModule, LikeModule, ReportModule, PaymentsModule, AdminModule, NoticeModule],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Planit API')
+    .setDescription('플래닛 여행 프로젝트 API 문서입니다.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 5001);
 }
 bootstrap();
+
+//커밋 메세지
+// feat - 추가
+// fix - 수정
