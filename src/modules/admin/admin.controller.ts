858f95cf41c6e54e29f54cb8158c53e36d255a59
hanbyeolson <nanbyeol7474@gmail.com>
2025-05-02 21:23:28 +0900

    Feat : 관리자 로그인 기능

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,15 +1,13 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { AdminLoginDto } from './dto/adminlogin.dto';
 
 @Controller('admin')
 export class AdminController {
-  constructor(private readonly adminService: AdminService) {}
+  constructor(private readonly authService: AdminService) {}
+
+  @Post('login')
+  async adminLogin(@Body() dto: AdminLoginDto) {
+    return this.authService.adminLogin(dto.email, dto.password);
+  }
 }
--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -3,8 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { Admin } from './entities/admin.entity';
+import { JwtModule } from '@nestjs/jwt';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Admin])],
+  imports: [
+    TypeOrmModule.forFeature([Admin]),
+    JwtModule.register({
+      secret: 'your_jwt_secret',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AdminController],
   providers: [AdminService],
 })
