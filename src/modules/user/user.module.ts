a85f8c4b4a2277aad9198a2a7125272fc537b15f
ash <tkdgys1234@gmail.com>
2025-05-08 20:31:46 +0900

    Fix: user.module 수정

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,8 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
+import { Payment } from '../payments/entities/payment.entity';
+import { Album } from '../album/entities/album.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Payment, Album])],
   controllers: [UserController],
   providers: [UserService],
 })
